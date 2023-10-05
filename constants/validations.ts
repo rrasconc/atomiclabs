02c522a6b42719711b3b99a8f2d11800433fe2d7
rrasconc <rigobertorascon5@gmail.com>
2023-10-05 16:08:39 -0700

    fix: phoneNumber validation

--- constants/validations.ts
+++ constants/validations.ts
@@ -3,5 +3,5 @@ import { object, string } from 'yup';
 export const signUpSchema = object({
   name: string().min(5, 'El nombre deberá tener mínimo 5 caracteres'),
   lastname: string().min(5, 'El apellido deberá tener mínimo 5 caracteres'),
-  phoneNumber: string().min(9, 'Ingresa un número celular válido'),
+  phoneNumber: string().min(10, 'Ingresa un número celular válido'),
 });
