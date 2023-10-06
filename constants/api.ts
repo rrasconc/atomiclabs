099ec9afd897d5f7b57c1d4879b38a36771ad837
rrasconc <rigobertorascon5@gmail.com>
2023-10-06 10:03:00 -0700

    feat: api retry request 3 times before throwing error

--- constants/api.ts
+++ constants/api.ts
@@ -4,3 +4,20 @@ export const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
   timeout: 15000,
 });
+
+api.interceptors.response.use(undefined, (error) => {
+  const { config } = error;
+
+  //if we have am error we retry the request 3 times
+  if (!config.retryCount) {
+    config.retryCount = 1;
+  } else if (config.retryCount < 3) {
+    config.retryCount += 1;
+  } else {
+    return Promise.reject(error);
+  }
+
+  // Retry the request after 1 second
+  const delay = new Promise((resolve) => setTimeout(resolve, 1000));
+  return delay.then(() => api(config));
+});
